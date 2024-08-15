9a0be1d53cfa2f2f5e6a6628b79261002f351cba
Prathyush <prathyush.webdev@gmail.com>
2024-08-15 16:27:53 +0530

    Made some changes

--- Sorting-Techniques/Selection.js
+++ Sorting-Techniques/Selection.js
@@ -16,5 +16,29 @@ function selectionSort(array) {
 }
 
 let arr = [13, 46, 24, 52, 20, 9]
+console.log("Array Before Sorting");
+console.log(arr);
+
+console.log("-----------------------------------");
+console.log("After Sorting");
+
+
 console.log(selectionSort(arr));
 // [ 9, 13, 20, 24, 46, 52 ]
+
+/*
+Pseudo Code :
+     for(i = 0 ; i < = length - 2; i++){
+          minVal = i;
+          for(j = i +1; j < = length - 1 ;j++){
+               if(arr[j] < arr[minVal]){  // If you want in Decending order just change if(arr[j] > arr[minVal])
+                    minVal = j;
+               }
+          }
+          // Swap the elements
+          temp = arr[minVal];
+          arr[minVal] = arr[i];
+          arr[i] = temp;
+     }
+
+*/
